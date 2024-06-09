38399ef284c9600a57b03ccd451741c52b529da5
Gerseneck <wang14240@gmail.com>
2024-06-09 13:45:55 -0400

    brightness slider easter egg

---
+++ js/after.js
@@ -0,0 +1,17 @@
+document.getElementById("modeButton").addEventListener("click", (event) => {
+    if (event.ctrlKey) {
+        new bootstrap.Modal(document.getElementById("brightnessModal")).show();
+        return;
+    }
+    let attribute = "data-bs-theme";
+    let currentState = document.documentElement.getAttribute(attribute);
+    let newState = currentState === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newState);
+
+    document.documentElement.setAttribute(attribute, newState);
+});
+
+let brightnessSlider = document.getElementById("brightnessRange");
+brightnessSlider.addEventListener("input", (event) => {
+    document.documentElement.style.filter = `brightness(${event.target.value / 100})`;
+});

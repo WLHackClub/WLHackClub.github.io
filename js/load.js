2ec7e93cc6ccb3df94aae413e6a6e58bfb1fa402
Gerseneck <wang14240@gmail.com>
2025-01-15 23:02:50 -0500

    feat: set light/dark mode to system defaults unless user changes

--- js/load.js
+++ js/load.js
@@ -1,3 +1,10 @@
-const getTheme = localStorage.getItem("theme") ?? "light";
+const getSystemDefaults = () => {
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+    }
+    return "light";
+}
+
+const getTheme = localStorage.getItem("theme") ?? getSystemDefaults()
 
 addEventListener("DOMContentLoaded", document.documentElement.setAttribute("data-bs-theme", getTheme));
